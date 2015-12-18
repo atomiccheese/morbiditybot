d272c1f062536159ab71303b94e80637150625a8
atomiccheese <nzentzis@gmail.com>
2015-12-17 23:04:55 -0800

    Update deathcounter and bot module infrastructure

--- src/bot.py
+++ src/bot.py
@@ -5,89 +5,176 @@ import random, traceback, os, os.path
 ERR_MSG = 'An error occurred in "%s" and it has been disabled. The MAGIC WORD is "%s".'
 
 class Bot(bot.SingleServerIRCBot):
-	def __init__(self, conf):
-		self.conf = conf
+        def __init__(self, conf):
+                self.conf = conf
 
-		# Construct mapping between channels and loadable modules
-		self.tojoin_channels = list(filter(lambda x: x[0] == '#',
-			conf.sections()))
-		self.chan_modules = {}
-		self.chan_mod_instances = {}
-		self.modules = {}
-		for c in self.tojoin_channels:
-			# Try to load a module if it isn't already available
-			mods = self.conf.get(c, 'modules').split(',')
-			for m in mods:
-				if m in self.modules:
-					continue
-				modpath = 'src.modules.%s' % m
-				self.modules[m] = importlib.import_module(modpath)
-				logging.info("Loaded module: %s", m)
-			self.chan_modules[c] = list(
-					map(lambda x: self.modules[x], mods))
+                # Construct mapping between channels and loadable modules
+                self.tojoin_channels = list(filter(lambda x: x[0] == '#',
+                        conf.sections()))
+                self.chan_modules = {} # Maps channel to list of module objects
+                self.chan_mod_instances = {} # Maps channel to {modname->instance}
+                self.modules = {} # Maps modname to module objects
 
-		# Establish connection to IRC server
-		srv = conf.get('Connection', 'server')
-		port = int(conf.get('Connection', 'port'))
-		user = conf.get('Connection', 'username')
+                for c in self.tojoin_channels:
+                        # Try to load a module if it isn't already available
+                        mods = self.conf.get(c, 'modules').split(',')
+                        for m in mods:
+                                if m in self.modules:
+                                        continue
+                                modpath = 'src.modules.%s' % m
+                                self.modules[m] = importlib.import_module(modpath)
+                                logging.info("Loaded module: %s", m)
+                        self.chan_modules[c] = list(
+                                        map(lambda x: self.modules[x], mods))
 
-		logging.info("Connecting to %s:%d user %s", srv, port, user)
+                # Establish connection to IRC server
+                srv = conf.get('Connection', 'server')
+                port = int(conf.get('Connection', 'port'))
+                user = conf.get('Connection', 'username')
 
-		srv = bot.ServerSpec(srv, port,
-				conf.get('Connection', 'oauth_password'))
-		bot.SingleServerIRCBot.__init__(self, [srv], user, user)
-	
-	def on_endofmotd(self, conn, evt):
-		logging.debug("Connected to server. MOTD ended.")
+                self.admins = conf.get('Connection', 'sys_admins').strip().split(',')
 
-		for c in self.tojoin_channels:
-			self.connection.join(c)
+                logging.info("Connecting to %s:%d user %s", srv, port, user)
 
-	def on_join(self, conn, evt):
-		chan = evt.target
-		logging.debug("Joined channel: %s", chan)
+                srv = bot.ServerSpec(srv, port,
+                                conf.get('Connection', 'oauth_password'))
+                bot.SingleServerIRCBot.__init__(self, [srv], user, user)
 
-		# Initialize channel modules
-		chanmods = self.chan_modules[evt.target]
-		instances = {}
-		for mod in chanmods:
-			mname = mod.__name__.split('.')[-1]
+        def load_module(self, mod, chan):
+            # Load the module if needed
+            if mod not in self.modules:
+                # Load the module if necessary
+                pth = 'src.modules.%s' % mod
+                self.modules[mod] = importlib.import_module(pth)
+                logging.info("Loaded module: %s" % mod)
 
-			# Build config dict
-			confdict = {}
-			for k in self.conf.options(chan):
-				prefix = mod.CONFIG_PREFIX
-				if not k.startswith(prefix + '_'):
-					continue
-				val = self.conf.get(chan, k)
-				confdict[k[len(prefix)+1:]] = val
-			
-			# Instantiate the module
-			instances[mname] = mod.ModuleMain(conn, chan, confdict)
-			time.sleep(2)
-		self.chan_mod_instances[chan] = instances
-		logging.info("Created all modules for channel: %s", chan)
+            # Plug the module into this channel
+            modname = mod
+            mod = self.modules[mod]
+            if mod not in self.chan_mod_instances[chan]:
+                # Instantiate the module
+                conf = self.get_module_conf(chan, mod)
+                inst = mod.ModuleMain(self.connection, chan, conf)
+                self.chan_modules[chan].append(mod)
+                self.chan_mod_instances[chan][modname] = inst
+                time.sleep(2)
+                self.connection.privmsg(chan, "Module loaded: %s" % modname)
 
-		conn.privmsg(chan, 'Bot ready. Modules loaded: %s' % (' '.join(instances.keys())))
+        def unload_module(self, mod, chan):
+            if mod not in self.modules:
+                return
+            mname = mod
+            mod = self.modules[mod]
+            if chan != None:
+                # Only unload from this channel
+                self.chan_modules[chan].remove(mod)
+                del self.chan_mod_instances[chan][mname]
+                self.connection.privmsg(chan, "Module unloaded: %s" % mname)
+            else:
+                users = list(filter(lambda x: mod in self.chan_modules[x],
+                    self.tojoin_channels))
+                for u in users:
+                    self.chan_modules[u].remove(mod)
+                    del self.chan_mod_instances[u][mname]
+                    self.connection.privmsg(u, "Module unloaded: %s" % mname)
 
-	def dump_exception(self):
-		if not os.path.exists('crash_logs'):
-			os.mkdir('crash_logs')
-		name = ''.join([random.choice('abcdefghijklmnopqrstuvwxyz') for x in range(16)])
-		with open(os.path.join('crash_logs', name), 'w') as f:
-			f.write(traceback.format_exc())
-		return name
-	
-	def on_pubmsg(self, conn, evt):
-		chan = evt.target
-		src = evt.source[:evt.source.find('!')]
-		content = evt.arguments[0]
+        def reload_module(self, mod):
+            users = list(filter(lambda x: mod in self.chan_modules[c],
+                self.tojoin_channels))
 
-		cmi = self.chan_mod_instances[chan]
-		for iname in cmi.keys():
-			inst = cmi[iname]
-			try:
-				inst.on_message(src, content)
-			except Exception as e:
-				magic = self.dump_exception()
-				conn.privmsg(chan, ERR_MSG % (iname, magic))
+        def get_module_conf(self, chan, mod):
+                confdict = {}
+                for k in self.conf.options(chan):
+                        prefix = mod.CONFIG_PREFIX
+                        if not k.startswith(prefix + '_'):
+                                continue
+                        val = self.conf.get(chan, k)
+                        confdict[k[len(prefix)+1:]] = val
+                return confdict
+
+        def on_endofmotd(self, conn, evt):
+                logging.debug("Connected to server. MOTD ended.")
+
+                for c in self.tojoin_channels:
+                        self.connection.join(c)
+
+        def on_join(self, conn, evt):
+                chan = evt.target
+                logging.debug("Joined channel: %s", chan)
+
+                # Initialize channel modules
+                chanmods = self.chan_modules[evt.target]
+                instances = {}
+                for mod in chanmods:
+                        mname = mod.__name__.split('.')[-1]
+
+                        # Build config dict
+                        confdict = self.get_module_conf(chan, mod)
+
+                        # Instantiate the module
+                        instances[mname] = mod.ModuleMain(conn, chan, confdict)
+                        time.sleep(2)
+                self.chan_mod_instances[chan] = instances
+                logging.info("Created all modules for channel: %s", chan)
+
+                conn.privmsg(chan, 'Bot ready. Modules loaded: %s' % (' '.join(instances.keys())))
+
+        def dump_exception(self):
+                if not os.path.exists('crash_logs'):
+                        os.mkdir('crash_logs')
+                name = ''.join([random.choice('abcdefghijklmnopqrstuvwxyz') for x in range(16)])
+                with open(os.path.join('crash_logs', name), 'w') as f:
+                        f.write(traceback.format_exc())
+                return name
+        
+        def on_pubmsg(self, conn, evt):
+                chan = evt.target
+                src = evt.source[:evt.source.find('!')]
+                content = evt.arguments[0]
+
+                if(content.startswith("!mbt")):
+                    self.process_metacommand(chan, src, content)
+                    return
+
+                cmi = self.chan_mod_instances[chan]
+                for iname in cmi.keys():
+                        inst = cmi[iname]
+                        try:
+                                inst.on_message(src, content)
+                        except Exception as e:
+                                magic = self.dump_exception()
+                                conn.privmsg(chan, ERR_MSG % (iname, magic))
+
+        def process_metacommand(self, chan, src, content):
+            if src not in self.admins:
+                conn.privmsg(chan, '[metacmd] You do not have system-level access')
+                return
+            parts = content.split(' ')[1:]
+            if len(parts) == 0:
+                conn.privmsg(chan, '[metacmd] No operation specified')
+                return
+            cmd = parts[0]
+            if cmd == 'unload':
+                if len(parts) < 2:
+                    conn.privmsg(chan, '[metacmd] Must specify module')
+                    return
+                opts = parts[2:]
+                for i in opts:
+                    if i not in ['force']:
+                        conn.privmsg(chan, '[metacmd] Unknown option: %s' % i)
+
+                if 'force' in opts:
+                    self.unload_module(parts[1], None)
+                else:
+                    self.unload_module(parts[1], chan)
+            if cmd == 'reload':
+                if len(parts) != 2:
+                    conn.privmsg(chan, '[metacmd] Must specify module')
+                    return
+                self.unload_module(parts[1], chan)
+                self.load_module(parts[1], chan)
+            if cmd == 'load':
+                if len(parts) != 2:
+                    conn.privmsg(chan, '[metacmd] Must specify module')
+                    return
+                self.load_module(parts[1], chan)
--- src/modules/deathcounter.py
+++ src/modules/deathcounter.py
@@ -4,174 +4,181 @@ CONFIG_PREFIX = "death"
 TWITCH_API = 'https://api.twitch.tv/kraken/'
 
 def print_num(n):
-	places = ['thousand', 'million', 'billion', 'trillion', 'quadrillion']
-	if n < 100:
-		digits = ['one','two','three','four','five','six','seven',
-				'eight','nine','ten','eleven','twelve',
-				'thirteen','fourteen','fifteen','sixteen',
-				'seventeen','eighteen','nineteen']
-		tens = ['twenty','thirty','forty','fifty','sixty','seventy',
-				'eighty','ninety']
-		if n < len(digits):
-			return digits[n-1]
-		else:
-			tens = tens[(n // 10)-2]
-			if n % 10 == 0:
-				return tens
-			ones = digits[n % 10 - 1]
-			return tens + ' ' + ones
-	elif n < 1000:
-		hundreds = print_num(n // 100)
-		ones = print_num(n % 100)
-		return hundreds + ' hundred ' + ones
-	else:
-		parts = []
-		idx = 0
-		while(n > 0):
-			if(idx > 0):
-				parts.append(places[idx-1])
-			parts.append(print_num(n % 1000))
-			n //= 1000
-			idx += 1
-		return ' '.join(parts[::-1])
+        places = ['thousand', 'million', 'billion', 'trillion', 'quadrillion']
+        if n < 100:
+                digits = ['one','two','three','four','five','six','seven',
+                                'eight','nine','ten','eleven','twelve',
+                                'thirteen','fourteen','fifteen','sixteen',
+                                'seventeen','eighteen','nineteen']
+                tens = ['twenty','thirty','forty','fifty','sixty','seventy',
+                                'eighty','ninety']
+                if n < len(digits):
+                        return digits[n-1]
+                else:
+                        tens = tens[(n // 10)-2]
+                        if n % 10 == 0:
+                                return tens
+                        ones = digits[n % 10 - 1]
+                        return tens + ' ' + ones
+        elif n < 1000:
+                hundreds = print_num(n // 100)
+                ones = print_num(n % 100)
+                return hundreds + ' hundred ' + ones
+        else:
+                parts = []
+                idx = 0
+                while(n > 0):
+                        if(idx > 0):
+                                parts.append(places[idx-1])
+                        parts.append(print_num(n % 1000))
+                        n //= 1000
+                        idx += 1
+                return ' '.join(parts[::-1])
 
 class ModuleMain:
-	def __init__(self, conn, channel, conf):
-		self.conf = conf
-		self.conn = conn
-		self.chan = channel
-		self.admins = self.conf['admins'].split(',')
+        def __init__(self, conn, channel, conf):
+                self.conf = conf
+                self.conn = conn
+                self.chan = channel
+                self.admins = self.conf['admins'].split(',')
+                self.happy = (self.conf['happy'].lower() == 'true')
 
-		self.deaths = 0
-		self.enabled = False
-		self.last_game = None
+                self.deaths = 0
+                self.enabled = False
+                self.last_game = None
 
-		# Find the game being played
-		try:
-			game = self.get_game()
-			self.load_deaths(game)
-		except IOError:
-			self.error('Unable to determine game. Death counter disabled.')
-			self.enabled = False
-	
-	def send(self, msg):
-		self.conn.privmsg(self.chan, msg)
-	
-	def error(self, msg):
-		self.status('error: %s' % msg)
-	
-	def status(self, msg):
-		self.send('[deathcounter] %s' % msg)
-	
-	def get_game(self):
-		hdrs = {'accept': 'application/vnd.twitchtv.v3+json'}
-		url = TWITCH_API+'channels/'+self.chan[1:]
-		r = requests.get(url, headers=hdrs)
-		r.raise_for_status()
-		return r.json()['game']
-	
-	def update_game(self):
-		try:
-			g = self.get_game()
-		except IOError:
-			self.error('Unable to determine game. Death counter disabled.')
-			self.enabled = False
-			return False
+                # Find the game being played
+                try:
+                        game = self.get_game()
+                        self.load_deaths(game)
+                except IOError:
+                        self.error('Unable to determine game. Death counter disabled.')
+                        self.enabled = False
+        
+        def send(self, msg):
+                self.conn.privmsg(self.chan, msg)
+        
+        def error(self, msg):
+                self.status('error: %s' % msg)
+        
+        def status(self, msg):
+                self.send('[deathcounter] %s' % msg)
+        
+        def get_game(self):
+                hdrs = {'accept': 'application/vnd.twitchtv.v3+json'}
+                url = TWITCH_API+'channels/'+self.chan[1:]
+                r = requests.get(url, headers=hdrs)
+                r.raise_for_status()
+                return r.json()['game']
+        
+        def update_game(self):
+                try:
+                        g = self.get_game()
+                except IOError:
+                        self.error('Unable to determine game. Death counter disabled.')
+                        self.enabled = False
+                        return False
 
-		if g != self.last_game:
-			self.load_deaths(g)
-		return True
-	
-	def load_deaths(self, game):
-		self.last_game = game
+                if g != self.last_game:
+                        self.load_deaths(g)
+                return True
+        
+        def load_deaths(self, game):
+                self.last_game = game
 
-		# Create user directory if it doesn't already exist
-		pth_game = game.replace('/', '\xff')
-		pth_chan = 'deaths_' + self.chan[1:]
-		if not os.path.exists(pth_chan):
-			os.mkdir(pth_chan)
-		pth = os.path.join(pth_chan, pth_game)
-		self.enabled = True
+                # Create user directory if it doesn't already exist
+                pth_game = game.replace('/', '\xff')
+                pth_chan = 'deaths_' + self.chan[1:]
+                if not os.path.exists(pth_chan):
+                        os.mkdir(pth_chan)
+                pth = os.path.join(pth_chan, pth_game)
+                self.enabled = True
 
-		# Open the data file
-		try:
-			with open(pth, 'r') as f:
-				self.deaths = int(f.read())
-		except IOError as e:
-			pass
-		except ValueError as e:
-			pass
-		self.status('Death count for %s: %d' % (game, self.deaths))
-	
-	def save_deaths(self):
-		pth_game = self.last_game.replace('/', '\xff')
-		pth_chan = 'deaths_' + self.chan[1:]
-		pth = os.path.join(pth_chan, pth_game)
-		with open(pth, 'w') as f:
-			f.write(str(self.deaths))
-	
-	def count_death(self):
-		self.deaths += 1
-		self.save_deaths()
-		return self.deaths
-	
-	def on_message(self, src, content):
-		content = content.strip()
-		words = content.split(' ')
-		if len(words) == 0:
-			return
-		cmd = words[0]
-		if cmd[0] != '!':
-			return
-		cmd = words[0][1:]
-		
-		if(hasattr(self, 'cmd_'+cmd)):
-			getattr(self, 'cmd_'+cmd)(src,
-					words[1:],
-					content[len(words[0]):].strip(),
-					src)
-	
-	def cmd_death(self, src, args, content, user):
-		if(len(args) == 0):
-			self.send("Error: !death requires arguments")
-			return
-		if src not in self.admins:
-			self.send("Error: !death requires module admin access")
-			return
-		if(args[0] == 'set' and len(args) == 2):
-			try:
-				self.deaths = int(args[1])
-			except ValueError as e:
-				self.send("Error: '%s' is not a valid number" % args[2])
-				return
-			self.save_deaths()
-			self.send("Number of deaths set to %d" % self.deaths)
-		elif args[0] == 'reload':
-			self.load_deaths(self.get_game())
-		elif args[0] == 'save':
-			self.save_deaths()
-		else:
-			self.send("Error: Unknown subcommand '%s'" % args[0])
-			return
+                # Open the data file
+                try:
+                        with open(pth, 'r') as f:
+                                self.deaths = int(f.read())
+                except IOError as e:
+                        pass
+                except ValueError as e:
+                        pass
+                self.status('Death count for %s: %d' % (game, self.deaths))
+        
+        def save_deaths(self):
+                pth_game = self.last_game.replace('/', '\xff')
+                pth_chan = 'deaths_' + self.chan[1:]
+                pth = os.path.join(pth_chan, pth_game)
+                with open(pth, 'w') as f:
+                        f.write(str(self.deaths))
+        
+        def count_death(self):
+                self.deaths += 1
+                self.save_deaths()
+                return self.deaths
+        
+        def on_message(self, src, content):
+                content = content.strip()
+                words = content.split(' ')
+                if len(words) == 0:
+                        return
+                cmd = words[0]
+                if cmd[0] != '!':
+                        return
+                cmd = words[0][1:]
+                
+                if(hasattr(self, 'cmd_'+cmd)):
+                        getattr(self, 'cmd_'+cmd)(src,
+                                        words[1:],
+                                        content[len(words[0]):].strip(),
+                                        src)
+        
+        def cmd_death(self, src, args, content, user):
+                if(len(args) == 0):
+                        self.send("Error: !death requires arguments")
+                        return
+                if src not in self.admins:
+                        self.send("Error: !death requires module admin access")
+                        return
+                if(args[0] == 'set' and len(args) == 2):
+                        try:
+                                self.deaths = int(args[1])
+                        except ValueError as e:
+                                self.send("Error: '%s' is not a valid number" % args[2])
+                                return
+                        self.save_deaths()
+                        self.send("Number of deaths set to %d" % self.deaths)
+                elif args[0] == 'reload':
+                        self.load_deaths(self.get_game())
+                elif args[0] == 'save':
+                        self.save_deaths()
+                else:
+                        self.send("Error: Unknown subcommand '%s'" % args[0])
+                        return
 
-	def cmd_rip(self, src, args, content, user):
-		if not self.update_game():
-			return
-		if not self.enabled:
-			self.error('Death counter is currently disabled')
+        def cmd_rip(self, src, args, content, user):
+                if not self.update_game():
+                        return
+                if not self.enabled:
+                        self.error('Death counter is currently disabled')
 
-		n = self.count_death()
-		if self.conf['spaced_text'].lower() == 'true':
-			disp = print_num(n).upper().replace(' ','')
-			final = ' '.join(disp + 'BOYS')
-		else:
-			final = str(self.deaths)
-		self.send(final)
+                n = self.count_death()
+                if self.conf['spaced_text'].lower() == 'true':
+                        disp = print_num(n).upper().replace(' ','')
+                        if self.happy:
+                            final = ' '.join(disp + 'HAPPYLITTLEACCIDENTS')
+                        else:
+                            final = ' '.join(disp + 'BOYS')
+                else:
+                        final = str(self.deaths)
+                self.send(final)
 
-	def cmd_deaths(self, src, args, content, user):
-		if not self.update_game():
-			return
-		if not self.enabled:
-			self.error('Death counter is currently disabled')
+        def cmd_deaths(self, src, args, content, user):
+                if not self.update_game():
+                        return
+                if not self.enabled:
+                        self.error('Death counter is currently disabled')
 
-		self.send("%s has died %d times" % (self.chan, self.deaths)) 
+                if self.happy:
+                    self.send("%s has had %d happy little accidents" % (self.chan, self.deaths)) 
+                else:
+                    self.send("%s has died %d times" % (self.chan, self.deaths)) 
